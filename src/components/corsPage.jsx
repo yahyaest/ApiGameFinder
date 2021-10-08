8772b597c5cc6622bc4212ff96bf967c552932e2
Yahya <machatyahya@gmail.com>
2021-10-08 20:23:49 +0100

    Add enable CORS page V3

--- src/components/corsPage.jsx
+++ src/components/corsPage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-function CrosPage() {
+function CorsPage() {
   return (
-   <div style={{ width: "90%", marginTop: "250px",textAlign:"center" }}>
+    <div style={{ width: "90%", marginTop: "250px", textAlign: "center" }}>
       <p>
-        This site uses igdb api that doesn't support cros origin. <br/> To enable CORS
-        please click on the button below. <br/> You'll be directed to
-        "https://cors-anywhere.herokuapp.com/corsdemo" to unlock access to igdb
-        api. <br/> Then refresh the site and everything will work perfectly. <br/> If you get redirect to this pagr again then you have riched the limit 50 api call per hour. <br/> Please retry after one hour.
+        This site uses igdb api that doesn't support cors origin. <br /> To
+        enable CORS please click on the button below. <br /> You'll be directed
+        to "https://cors-anywhere.herokuapp.com/corsdemo" to unlock access to
+        igdb api. <br /> Then refresh the site and everything will work
+        perfectly. <br /> If you get redirect to this pagr again then you have
+        riched the limit 50 api call per hour. <br /> Please retry after one
+        hour.
       </p>
 
       <button
@@ -17,10 +20,10 @@ function CrosPage() {
           window.open("https://cors-anywhere.herokuapp.com/corsdemo")
         }
       >
-        Enable CROS
+        Enable CORS
       </button>
     </div>
   );
 }
 
-export default CrosPage;
+export default CorsPage;
